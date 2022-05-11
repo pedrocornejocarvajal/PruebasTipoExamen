b498c77c32149850cba0c2ced699b617d33e9558
pcornejo <pedro.cornejo@iesnervion.es>
2022-05-11 11:51:47 +0200

    Initial commit

---
+++ src/Clases/EjerciciosVector.java
@@ -0,0 +1,49 @@
+public class Vector {
+
+    public double[] vector;
+
+    public  Vector(double[] vector){
+
+        this.vector = vector.clone();
+    }
+
+    public void duplicarValores() throws Exception {
+        //multiplica valores por 2
+        if (vector.length==0) {
+            throw new Exception("El vector no esta iniciado");
+        }
+            for (int i = 0; i < vector.length; i++) {
+                vector[i] = vector[i * 2];
+
+        }
+    }
+
+
+    public double obtenerMaximo() throws Exception {
+        double maximo = vector[0];
+        if (vector.length==0) {
+            throw new Exception("El vector no esta iniciado");
+        }
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i]> maximo){
+                maximo = vector[i];
+            }
+        }
+        return maximo;
+    }
+
+    public boolean contieneValor(double valor) throws Exception {
+        boolean resultado=false;
+        int i=0;
+        if (vector.length==0) {
+            throw new Exception("El vector no esta iniciado");
+        }
+        while(!resultado && i<vector.length){
+            if (vector[i] == valor){
+                resultado = true;
+            }
+            i++;
+        }
+        return resultado;
+    }
+}
---
+++ src/Test/EjerciciosVectorTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @org.junit.jupiter.api.Test
+    void duplicarValores() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void obtenerMaximo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contieneValor() {
+    }
+}
